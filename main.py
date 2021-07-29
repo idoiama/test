21af2b1255432f0735e4929296b42ed5a901464b
idoiama <47849224+idoiama@users.noreply.github.com>
2021-07-29 15:46:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-
-import streamlit as st
 from matplotlib import pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
